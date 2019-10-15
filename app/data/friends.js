6df195aa28796301333f786d44522ef1cd7d9a31
Alan Leverenz <awleverenz@aol.com>
2019-10-15 16:04:37 -0400

    app in first commit status. Remaining task is to create friend array match function.

---
+++ app/data/friends.js
@@ -0,0 +1,8 @@
+var userData = [{
+    name: "Alan",
+    photo: "http://getwallpapers.com/wallpaper/full/2/f/e/501038.jpg",
+    scores: [ 5,1,4,4,5,1,2,5,4,1 ]
+  }];
+
+module.exports = userData;
+
